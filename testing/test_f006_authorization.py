d43d4363d8d319b519ba96f7a38de4c86000d5e6
Farah Hanim <farahhanim8727@icloud.com>
2026-06-08 19:56:29 +0800

    F-006 Authorization BVA Testing

--- testing/test_f006_authorization.py
+++ testing/test_f006_authorization.py
@@ -1,5 +1,5 @@
 import pytest
-
+from server.forms import LoginForm
 
 # ============================================================
 # DTT_002
@@ -120,3 +120,82 @@ def test_TC_F006_DTT_008_patient_denied_medtest_upload(
     )
 
     assert response.status_code in [302, 403]
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_001_email_length_49():
+    email = ("a" * 37) + "@test.com"  # total length 46-49 range
+
+    form = LoginForm(
+        data={
+            "email": email,
+            "password": "Password123"
+        }
+    )
+
+    assert form.is_valid() is False
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_002_email_length_50():
+    email = ("a" * 38) + "@test.com"
+
+    form = LoginForm(
+        data={
+            "email": email,
+            "password": "Password123"
+        }
+    )
+
+    assert form.is_valid() is False
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_003_email_length_51():
+    email = ("a" * 39) + "@test.com"
+
+    form = LoginForm(
+        data={
+            "email": email,
+            "password": "Password123"
+        }
+    )
+
+    assert form.is_valid() is False
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_004_password_length_49(
+    patient_account
+):
+    form = LoginForm(
+        data={
+            "email": patient_account.user.username,
+            "password": "a" * 49
+        }
+    )
+
+    assert len(form["password"].value()) == 49
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_005_password_length_50(
+    patient_account
+):
+    form = LoginForm(
+        data={
+            "email": patient_account.user.username,
+            "password": "a" * 50
+        }
+    )
+
+    assert len(form["password"].value()) == 50
+
+@pytest.mark.django_db
+def test_TC_F006_BVA_006_password_length_51(
+    patient_account
+):
+    form = LoginForm(
+        data={
+            "email": patient_account.user.username,
+            "password": "a" * 51
+        }
+    )
+
+    assert not form.is_valid()
+    assert "password" in form.errors
